7d7e868d5da3c0d953d68b645c919ae25f3a1634
Bharti152k2 <nidhi15sak@gmail.com>
2024-07-22 22:41:29 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,29 +13,56 @@ import Expenses from "./components/Expenses";
 import Spendings from "./components/Spendings";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    fetch(`${apiUrl}/your-endpoint`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
-    <Authentication>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/expenses"
-            element={
-              <ProtectedRoute>
-                <Expenses />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/spendings" element={<Spendings />} />
-          <Route path="/addexpenses" element={<AddExpenses />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/updatecategory/:id" element={<UpdateCategory />} />
-        </Routes>
-      </BrowserRouter>
-    </Authentication>
+    <>
+      <div>
+        <h1>Data from API:</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+      <Authentication>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/expenses"
+              element={
+                <ProtectedRoute>
+                  <Expenses />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/spendings" element={<Spendings />} />
+            <Route path="/addexpenses" element={<AddExpenses />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/updatecategory/:id" element={<UpdateCategory />} />
+          </Routes>
+        </BrowserRouter>
+      </Authentication>
+    </>
   );
 }
 
