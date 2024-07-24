57573a735497fb127ddb520a0be57eb6544b0d5b
Bharti <160716872+Bharti152k2@users.noreply.github.com>
2024-07-24 15:30:33 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,33 +13,8 @@ import Expenses from "./components/Expenses";
 import Spendings from "./components/Spendings";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const apiUrl = "https://expense-tracker-backend-ufdt.onrender.com";
-
-    fetch(`${apiUrl}/your-endpoint`)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
   return (
     <>
-      <div>
-        <h1>Data from API:</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
       <Authentication>
         <BrowserRouter>
           <Navbar />
